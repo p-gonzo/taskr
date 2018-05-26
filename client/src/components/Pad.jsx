e4fcc6274b1520aed27898c21690c262aac8d1ec
Philip Gonzalez <pgonzalez115@gmail.com>
2018-05-25 20:32:53 -0400

    Add button to delete pad

--- client/src/components/Pad.jsx
+++ client/src/components/Pad.jsx
@@ -54,6 +54,7 @@ class Pad extends React.Component {
         data-x = {this.props.posX}
         data-y = {this.props.posY}
       >
+        <button className='close-pad'>X</button>
         <PadInput id={this.props.id} getTasks={this.getTasks} />
         <Contents todos={this.state.tasks} handleTaskClick={this.handleTaskClick}/>
       </div>
